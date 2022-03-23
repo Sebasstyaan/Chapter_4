dba2631d20e4ce917424ac8adcbe95477532379e
Sebasstyaan <scvargas40@misena.edu.co>
2022-03-23 09:43:50 -0500

    Ejercicio
    Ejercicio

--- Ejercicio_2.py
+++ Ejercicio_2.py
@@ -1,7 +1,23 @@
-is_cold=(input("Digite: "))
-is_cold=True and False
-if is_cold==True:
-    print("Coge chaqueta")
-else:
-    if is_cold==False:
-        print("Usa camiseta")
+puede_volar=bool(input('¿El heroe puede volar?: '))
+puede_volar=True and False
+es_humano=bool(input('¿El heroe es humano?: '))
+es_humano=True and False
+tiene_mascara=bool(input('¿El heroe tiene mascara?: '))
+tiene_mascara=True and False
+
+if puede_volar is True and es_humano is True and tiene_mascara is True:
+    print('El Heroe Es Iroman')
+elif puede_volar is True and es_humano is True and tiene_mascara is False:
+    print('El Heroe Es Capitan Marvel')
+elif puede_volar is True and es_humano is False and tiene_mascara is True:
+    print('El Heroe Es Ronan Accuser')
+elif puede_volar is True and es_humano is False and tiene_mascara is False:
+    print('El Heroe Es Vision')
+elif puede_volar is False and es_humano is True and tiene_mascara is True:
+    print('El Heroe Es Spiderman')
+elif puede_volar is False and es_humano is True and tiene_mascara is False:
+    print('El Heroe Es Hulk')
+elif puede_volar is False and es_humano is False and tiene_mascara is True:
+    print('El Heroe Es Black Bolt')
+elif puede_volar is False and es_humano is False and tiene_mascara is False:
+    print('El Heroe Es Thanos')
---
+++ En_clase/Tienda.py
@@ -0,0 +1,27 @@
+#En la tienda de “Milena “se ha implementado la siguiente estrategia de bonificación al cliente:
+#Por compras superiores a $50.000, el cliente puede sacar de una bolsa, una balota y 
+# de acuerdo al color obtener un descuento en el valor a pagar:
+#Si la balota es roja; el cliente obtendrá el 100% de descuento
+#Si la balota es verde; el cliente obtendrá el 50% de descuento
+#Si la balota es blanca; el cliente obtendrá un 30% de descuento
+#Si la balota es negra; el cliente obtendrá un 20% de descuento
+#Si la balota es amarilla; el cliente obtendrá un 10% de descuento
+#Imprimir el valor de la balota, el valor del descuento, y el valor a pagar aplicado el descuento.
+#Si no aplica el descuento, se debe imprimir el mensaje 	que no aplica descuento y el valor a pagar
+#print("Gran dia de promocion en nuestra tienda Milena.Por compras superiores a $50000 podras participar\
+#en el descuento desde 100% y hasta el \
+#10% de descuento. La mecanica del juego consiste en que el cliente\
+#al pasar por la caja registradora y el monto de sus productos es superior a $50000 pesos\
+    #podra sacar una balota de nuestra bolsa sorpresa y dependiendo del color de la balota que saque asi\
+    #aplicara el descuento en caja")
+evento=True and False
+roja=100
+verde=0.50
+blanca=0.30
+negra=0.20
+amarilla=0.10
+compra=int(input("Digite el valor de la compra: "))
+if compra>50000:
+    evento=bool(input("Desea participar en la promocion?: "))
+    if evento==True
+    
