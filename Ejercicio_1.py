cb046642a38e8bf0de976785da7822986d5bb50d
Sebasstyaan <scvargas40@misena.edu.co>
2022-03-28 18:48:34 -0500

    Ejercicios
    Ejercicios

--- Ejercicio_1.py
+++ Ejercicio_1.py
@@ -3,8 +3,9 @@
 #Entrada: 2008 Salida: Es un año bisiesto
 
 año=int(input("Digite el año: "))
-if año % 4==0 and año % 400==0:
+if año % 4==0 and año % 100!=0:
     print('El año es bisiesto')
+elif año % 400==0:
+    print("El año  es bisiesto")
 else:
-    año % 100 != 0
-    print('El año no es bisiesto')
+    print("El año no es bisiesto")
--- Ejercicios_de_repaso/Ejercicio_2.py
+++ Ejercicios_de_repaso/Ejercicio_2.py
@@ -1,13 +1,13 @@
 #Escriba un programa en Python que acepte una cadena de texto e indique si todos sus caracteres son alfabéticos
 contar=0
-Alfabeto=('aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVxXyYzZ')
-Texto=input('Digite la palabra: ')
+texto=input("Digite el texto: ")
+alfabeto='aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVxXyYzZ'
 
-for i in Texto:
-    if Texto in Alfabeto:
-        contar+=1        
-else:
-    print("Se ha encontrado caracteres No alfabeticos")
-    
-if contar==len(Texto):
+for i in texto:
+    if i in alfabeto:
+        contar+=1
+    else:
+        print("Se ha encontrado caracteres No alfabeticos")
+    break
+if contar==len(texto):
     print("Todos los caracteres son alfabeticos")        
